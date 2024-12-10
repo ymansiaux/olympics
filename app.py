04adcd59dfa0d126462319492280dcebecfad2e4
yohann <yohann@thinkr.fr>
2024-12-10 11:47:57 +0100

    wip: fixing db

--- app.py
+++ app.py
@@ -11,12 +11,14 @@ if "db" not in os.listdir():
     os.mkdir("db")
 
 if "olympics.duckdb" not in os.listdir("db"):
+    logging.message("creating db")
     exec(open("import_data/data.py").read())
 
 con = duckdb.connect(database="db/olympics.duckdb", read_only=False)
 
 
-for tbl in ["athletes", "medals_total", "medals", "teams", "nocs"]:
+# for tbl in ["athletes", "medals_total", "medals", "teams", "nocs"]:
+for tbl in ["athletes"]:
     # breakpoint()
     df_table = con.execute(f"SELECT * FROM '{tbl}' LIMIT 5").df()
     st.dataframe(df_table)
