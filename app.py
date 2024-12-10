64807a8c6a44cb20418493f255d3c74371f88074
yohann <yohann@thinkr.fr>
2024-12-10 11:30:13 +0100

    init db logic

---
+++ app.py
@@ -0,0 +1,24 @@
+import os
+import logging
+import duckdb
+import streamlit as st
+
+
+if "db" not in os.listdir():
+    print("creating folder db")
+    logging.error(os.listdir())
+    logging.error("creating folder db")
+    os.mkdir("db")
+
+if "olympics.duckdb" not in os.listdir("db"):
+    exec(open("import_data/data.py").read())
+
+con = duckdb.connect(database="db/olympics.duckdb", read_only=False)
+
+
+for tbl in ["athletes", "medals_total", "medals", "teams", "nocs"]:
+    df_table = con.execute(f"SELECT * FROM {tbl} LIMIT 5").df()
+    st.dataframe(df_table)
+
+
+con.close()
